9f0c3d8e34bc6cf49eb99bb243448039bf42e22a
David Thomason <dtom90@users.noreply.github.com>
2021-02-25 20:43:14 -0800

    Created GroceryList app with CloudKit

--- MyGroceryList/ViewController.swift
+++ MyGroceryList/ViewController.swift
@@ -5,15 +5,94 @@
 //  Created by David Thomason on 2/25/21.
 //
 
+import CloudKit
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
+    private let database = CKContainer(identifier: "iCloud.com.davidthomason.MyGroceryList").publicCloudDatabase
+    
+    var items = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Grocery List"
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        let control = UIRefreshControl()
+        control.addTarget(self, action: #selector(fetchItems), for: .valueChanged)
+        tableView.refreshControl = control
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(didTapAdd))
+        fetchItems()
+    }
+    
+    @objc func fetchItems() {
+        let query = CKQuery(recordType: "GroceryItem", predicate: NSPredicate(value: true))
+        database.perform(query, inZoneWith: nil) { [weak self] records, error in
+            guard let records = records, error == nil else {
+                return
+            }
+            DispatchQueue.main.async {
+                self?.items = records.compactMap({ $0.value(forKey: "name") as? String })
+                self?.tableView.reloadData()
+            }
+        }
+    }
+    
+    @objc func didTapAdd() {
+        let alert = UIAlertController(title: "Add Item",
+                                      message: nil,
+                                      preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = "Enter Name..."
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { _ in
+            if let field = alert.textFields?.first, let text = field.text, !text.isEmpty {
+                self.saveItem(name: text)
+            }
+        }))
+        present(alert, animated: true)
+    }
+    
+    @objc func saveItem(name: String) {
+        let record = CKRecord(recordType: "GroceryItem")
+        record.setValue(name, forKey: "name")
+        database.save(record) { record, error in
+            if record != nil, error == nil {
+                DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {
+                    self.fetchItems()
+                })
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
 
+    // MARK: - Table
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
+                                                 for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
 }
 
